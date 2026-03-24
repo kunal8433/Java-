51e860b2491bdeee8661a3e3baa3e5cd51a67428
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-03-24 19:21:25 +0530

    Add files via upload

---
+++ type_1.java
@@ -0,0 +1,11 @@
+public class type_1 {
+    public static void main(String a[])
+    {
+       int num1 = 3;
+       int num2 = 7;
+       int result = num1 + num2;
+        System.out.println(result);
+    }
+    
+}
+
