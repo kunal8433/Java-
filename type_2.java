00cd09192f9f03240ad8ebb47cd0262703d1e597
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-03-24 19:21:12 +0530

    Add files via upload

---
+++ type_2.java
@@ -0,0 +1,18 @@
+
+public class type_2 {
+    public static void main(String a[])
+    {
+
+        int num = 12;
+        double num2 = 123.33;
+        char c = 'k';
+
+        boolean b  = true;
+        
+        System.out.println(num);
+        System.out.println(num2);
+        System.out.println(c);
+        System.out.println(b);
+    
+    }
+}
