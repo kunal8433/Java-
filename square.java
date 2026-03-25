a26653efc4bec470f49dfd17ec0589c5245857e8
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-03-25 18:58:02 +0530

    Add files via upload

---
+++ square.java
@@ -0,0 +1,12 @@
+public class square {
+    public static void main (String a[])
+    {
+        int num1 = 20;
+        int num2 =10;
+        int result = num1 % num2 ;
+
+        System.out.println(result);
+       
+    }
+    
+}
