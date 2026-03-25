b277716361c2d0f3282033e0630a11df6c197136
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-03-25 18:55:42 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,13 @@
+class add {
+    public static void main(String a[])
+    {
+        int num1 = 12;
+        int num2 = 8 ;
+
+        int result = num1 + num2 ;
+        System.out.println(result);
+
+        
+       
+    }
+}
