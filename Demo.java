37059271584e3d3d74fb4507b3fc31f7a66a2c32
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-03-25 18:56:00 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,12 @@
+public class Demo {
+    public static void main (String a[])
+    {
+        int x = 10;
+        int y = 20;
+        //boolean
+        boolean result = x > y ;
+
+        System.out.println(result);
+    }
+    
+}
