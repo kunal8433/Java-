4a6fec770f6e0f7db2da17e372f743d871163327
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-03-25 18:58:29 +0530

    Add files via upload

---
+++ sub.java
@@ -0,0 +1,11 @@
+public class sub {
+    public static void main (String a[])
+    {
+        int num1 = 20;
+        int num2 = 8;
+        int result = num1 - num2 ;
+
+        System.out.println(result);
+    }
+    
+}
