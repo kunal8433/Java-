5d061d0b8ba63846a3c388a012d3cb62aab1bdf8
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-03-25 18:56:16 +0530

    Add files via upload

---
+++ Divide.java
@@ -0,0 +1,13 @@
+public class Divide {
+    public static void main(String a[])
+    {
+        int num1 = 20;
+        int num2 = 10;
+
+        int result = num1 /num2 ;
+
+        System.out.println(result);
+
+    }
+    
+}
