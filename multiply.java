cd493f0a42cbc999598ebc7e32e007d680323fca
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-03-25 18:57:31 +0530

    Add files via upload

---
+++ multiply.java
@@ -0,0 +1,11 @@
+public class multiply {
+    public static void main(String a[])
+    {
+        int num1 = 2 ;
+        int num2 = 5;
+        int result = num1*num2;
+
+        System.out.println(result);
+    }
+    
+}
