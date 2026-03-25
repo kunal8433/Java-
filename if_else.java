26af597761906fdc26efa633d1a7031eda3ef386
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-03-25 18:57:03 +0530

    Add files via upload

---
+++ if_else.java
@@ -0,0 +1,14 @@
+public class if_else {
+    public static void main (String a[])
+    {
+        int x = 18;
+
+        if (x>10){
+            System.out.println("Grater");
+            System.out.println("kunal");}
+        else
+
+        System.out.println("Smaller");
+    }
+    
+}
