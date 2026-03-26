431bd67a76a8126b5d33d997fac709a0bee2f274
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-03-26 12:21:18 +0530

    Add files via upload

---
+++ PS.java
@@ -0,0 +1,14 @@
+public class PS {
+    public static void main (String a[])
+    {
+        int i = 1;
+
+        while(i<=8)
+        {
+            System.out.println("Kunal" + i);
+        i++;
+
+    }
+    
+}
+}
