d903350eb141f16dac1c278575e648dc497bcd9c
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-03-26 12:21:59 +0530

    Add files via upload

---
+++ while_loop.java
@@ -0,0 +1,15 @@
+class while_loop{
+    public static void main (String a[])
+    {
+        int i = 1;
+        while(i<=4)
+        {
+            System.out.println("kunal" + i);
+            i++;
+            System.out.println("Malik"+i);
+        
+
+        }
+
+    }
+}
