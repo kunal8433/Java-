2d892d066a7ea56a01d50709962e19d3786125b0
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-03-26 12:20:24 +0530

    Add files via upload

--- Demo.java
+++ Demo.java
@@ -1,12 +1,14 @@
 public class Demo {
-    public static void main (String a[])
+    public static void main(String[] args)
     {
-        int x = 10;
-        int y = 20;
-        //boolean
-        boolean result = x > y ;
+        int x = 4;
+        int result = 0;
+
+        if (x % 2 == 0)
+            result = 10;
+        else
+            result = 20;
 
         System.out.println(result);
     }
-    
 }
