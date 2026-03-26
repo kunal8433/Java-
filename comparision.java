5a9b52f25db0a340e75d239131428a6ad7408a11
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-03-26 12:20:07 +0530

    Add files via upload

---
+++ comparision.java
@@ -0,0 +1,15 @@
+public class comparision {
+    public static void main (String a[])
+    {
+        int x = 4;
+        int result = 0;
+        if (x % 2 == 0)
+            result = 10;
+        else
+            result = 20;
+        System.out.println(result);
+
+
+    }
+    
+}
