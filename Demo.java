451088f43712333e08b912e46f3da4433efb26af
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-03-28 22:35:31 +0530

    Add files via upload

--- Demo.java
+++ Demo.java
@@ -1,14 +1,24 @@
-public class Demo {
-    public static void main(String[] args)
-    {
-        int x = 4;
-        int result = 0;
 
-        if (x % 2 == 0)
-            result = 10;
+class Computer {
+    public void playMusic() {
+        System.out.println("Music play");
+    }
+
+    public String getMeApen(int cost) {
+        if (cost >= 10)
+            return "Pen";
         else
-            result = 20;
+            return "None";
+    }
+}
 
-        System.out.println(result);
+public class Demo {
+    public static void main(String[] args) {
+        Computer obj = new Computer();
+        
+        obj.playMusic();   // correct method call
+
+        String str = obj.getMeApen(2);  // correct argument passing
+        System.out.println(str);
     }
 }
