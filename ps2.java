5c59a5e311b7fd80ef70554e97f8353431d70194
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-03-29 22:01:36 +0530

    Add files via upload

--- ps2.java
+++ ps2.java
@@ -1,13 +1,22 @@
-class ps2 {
+class add {
     public static void main (String [] args)
     {
-        int i = 1;
-        while(i<=8)
-        {
-        System.out.println("kunal");
-        i++;
-        }
-
-
+    int x = 13;
+    int  y = 13;
+    int result = x + y ;
+    System.out.println(result);
+    
     }
+    
+    
+}
+public class ps2  {
+    public static void main (String [] args)
+    {
+        int num1 = 12;
+        int num2 = 14;
+        int result = num1  + num2 ;
+        System.out.println(result);
+    }
+    
 }
