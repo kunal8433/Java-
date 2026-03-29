913dfbfa01831147d39f2326fad45be71fab272a
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-03-29 22:02:01 +0530

    Add files via upload

---
+++ ps4.java
@@ -0,0 +1,16 @@
+
+    public class ps4 {
+    public static void main(String[] args) {
+        int x = 1;
+        int i = 0;
+
+        if (x <= 4)
+            System.out.println("Kunal " + i);
+        else
+            System.out.println("jaat");
+
+        i++; // increment
+    }
+}
+    
+
