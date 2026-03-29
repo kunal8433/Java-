a6edf0095a230b48ba994fd199c803c1e9076aac
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-03-29 22:01:49 +0530

    Add files via upload

--- ps3.java
+++ ps3.java
@@ -1,8 +1,13 @@
 public class ps3 {
-    public static void main (String [] args)
-    {
-        for(int i=1 ; i<=4 ; i++)
-            System.out.println("Kunal");
+    public static void main (String [] args){
+        int x = 1;
+        int  i = 0;
+        if (x<=9){
+            System.out.println("Kunal" + i  );}
+        else 
+            System.out.println("jaat");
+         i++;
+        
     }
     
 }
