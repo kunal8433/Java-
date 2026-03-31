b2f6f3ac628d90fa5599bfeee38ebadb8c171b49
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-03-31 16:58:39 +0530

    Add files via upload

--- ps3.java
+++ ps3.java
@@ -1,13 +1,19 @@
 public class ps3 {
-    public static void main (String [] args){
-        int x = 1;
-        int  i = 0;
-        if (x<=9){
-            System.out.println("Kunal" + i  );}
-        else 
-            System.out.println("jaat");
-         i++;
+    public static void main(String[] args) {
         
-    }
-    
+        int x = 10;
+        int y = 10;
+        int z = 10;
+
+        if(x>y&&x>z)
+            System.out.println("X IS GRATER THAN Y AND Z ");
+        else if (y>x&&y>z)
+        System.out.println("Y IS GRATER THAN X AND Z");  
+        else if (z>x && z>y)
+            System.out.println("Z IS GRATER THAN X AND Y");
+        else if (x==y&&x==z&&y==z)
+            System.out.println("ALL ARE SAME");
+        else
+            System.out.println("error");
+      }
 }
