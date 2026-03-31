1a732f38ee9a4c85cbb53231d3d69145e6edd967
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-03-31 16:58:52 +0530

    Add files via upload

---
+++ room.java
@@ -0,0 +1,13 @@
+public class room {
+    public static void main(String[] args)
+    {
+        int  x = 18;
+        if (x>10)
+            System.out.println("Kunal");
+        else
+            System.out.println("hellow");
+        
+    }
+
+    
+}
