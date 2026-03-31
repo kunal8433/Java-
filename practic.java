f155e5f890c75643f92eedb314ac1fff35a504fb
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-03-31 16:57:52 +0530

    Add files via upload

---
+++ practic.java
@@ -0,0 +1,13 @@
+public class practic {
+    public static void main(String[] args) {
+        
+        int x = 1;
+
+        if (x>11)
+            System.out.println("kunal");
+
+        else
+            System.out.println("by");
+    }
+    
+}
