77829cdb1386c60bdbcdb46f86d72ce736873aba
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-03-31 16:58:28 +0530

    Add files via upload

---
+++ practice_2.java
@@ -0,0 +1,20 @@
+public class practice_2 {
+    public static void main(String[] args) {
+
+        int x = 12;
+        int y = 13;
+{
+        if (x>y )
+            System.out.println("X IS GRATER THAN Y");
+        else if (x==y)
+            System.out.println("BOTH ARE EQUAL");
+        else if (y>x)
+            System.out.println("Y IS GRATER THAN X");
+        else
+            System.out.println("ERROR");
+    }
+    
+        
+    }
+    
+}
