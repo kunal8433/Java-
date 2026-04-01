9b077638865747b96f31d50ccbc1b0de8c70d205
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-04-01 23:20:35 +0530

    Add files via upload

---
+++ problem1.java
@@ -0,0 +1,27 @@
+public class problem1 {
+    public static void main(String[] args) {
+        
+       int num = 29;
+
+    if (num%2==0)
+    {
+        System.out.println("THE NUM IS EVEN");
+        System.out.println("Thank you!");
+    }
+
+    else
+    {
+        System.out.println("THE NUM IS ODD");
+        System.out.println("Thank you!");
+    }
+
+    
+
+
+
+
+    
+    }
+
+    
+}
