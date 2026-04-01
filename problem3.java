6c4197e92020a917d8350a1d8196233a8839f815
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-04-01 23:21:09 +0530

    Add files via upload

---
+++ problem3.java
@@ -0,0 +1,29 @@
+public class problem3 {
+    public static void main(String[] args) {
+        int x = 100;
+        int y = 90;
+
+        if (x==y)
+            System.out.println("BOTH ARE EQUAL !");
+        else
+            if (x>y)
+            {
+                System.out.println("X  IS GREATER THAN Y !");
+                System.out.println("Thank you!!");
+            }
+
+            else if (y>x)
+            {
+                System.out.println("Y IS GREATER THAN X");
+                System.out.println("Thank you!!");
+            }
+            else
+            {
+                System.out.println("Something went wrong!");
+                System.out.println("Thank you!!");
+            }
+
+            
+    }
+    
+}
