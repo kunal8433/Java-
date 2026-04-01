10f1bbd720802a7ac3bd4b0759fc01d6bf4650cf
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-04-01 23:21:52 +0530

    Add files via upload

---
+++ problem4.java
@@ -0,0 +1,24 @@
+public class problem4 {
+    public static void main(String[] args) {
+        
+        int age = 19;
+
+        if (age>=18)
+
+        {
+            System.out.println("ELIGIBIL FOR VOTE!");
+            System.out.println("Thank you !!");
+        }
+
+        else
+            
+        {
+            System.out.println("NOT ELIGIBLE FOR VOTE!");
+            System.out.println("Thank you !!");
+        }
+
+    }
+}
+            
+
+
