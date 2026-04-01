54e508d6d7d01ba270d5ca624671ff9fb355e28b
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-04-01 23:20:48 +0530

    Add files via upload

---
+++ problem2.java
@@ -0,0 +1,16 @@
+public class problem2 {
+    public static void main(String[] args) {
+        
+        int num  = 0;
+        if (num<0)
+            System.out.println("Negative number!");
+        else if (num>0)
+            System.out.println("Number is positive!");
+        else if (num==0)
+            System.out.println("The number is zero!");
+        else
+            System.out.println("Something went wrong!");
+    }
+
+    
+}
