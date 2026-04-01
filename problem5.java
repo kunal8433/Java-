3120d6dcfb7508ed272c461fc038805df39b1e04
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-04-01 23:22:34 +0530

    Add files via upload

---
+++ problem5.java
@@ -0,0 +1,24 @@
+public class problem5 {
+    public static void main(String[] args) {
+        
+        int marks= 82;
+
+        if (marks>=33)
+
+        {
+            System.out.println("PASS!");
+            System.out.println("Thank you !!");
+        }
+
+        else
+            
+        {
+            System.out.println("FAIL");
+            System.out.println("Thank you !!");
+        }
+
+    }
+}
+            
+
+
