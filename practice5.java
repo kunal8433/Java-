1caec3fb46d02eea9c54981fbd7fa7d65a3c263c
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-04-02 20:32:36 +0530

    Add files via upload

---
+++ practice5.java
@@ -0,0 +1,14 @@
+public class practice5 {
+    public static void main(String[] args) {
+        
+        int num[]= new int[4];
+        num [0] = 1;
+        num[1] = 2;
+        num[3] = 4;
+        num[2] = 3;
+
+        System.out.println(num[2]);
+
+    }
+    
+}
