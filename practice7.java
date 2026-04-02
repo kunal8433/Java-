887077246781111bb5bf7a3d342f54960b451d11
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-04-02 20:33:48 +0530

    Add files via upload

---
+++ practice7.java
@@ -0,0 +1,17 @@
+public class practice7 {
+     public static void main(String[] args) {
+        
+        int num[]= new int[4];
+        num [0] = 1;
+        num[1] = 2;
+        num[3] = 4;
+        num[2] = 3;
+
+        for (int i = 0 ; i<= 3; i++)
+        {
+        System.out.println(num[i]);
+        }
+
+    
+}
+}
