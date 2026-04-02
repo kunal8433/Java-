22a683bc47bf23a5eb2a14e1bbe581be11ce76bb
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-04-02 20:32:16 +0530

    Add files via upload

---
+++ practice3.java
@@ -0,0 +1,18 @@
+public class practice3 {
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        while (i<6)
+        {
+            
+            System.out.println("kunal" + i);
+            i++;
+
+        
+        }
+        
+        
+    }
+    
+}
