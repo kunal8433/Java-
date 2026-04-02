1215a52eb5272013aeef025d4a5f039efdcc87d3
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-04-02 20:31:40 +0530

    Add files via upload

---
+++ practice1.java
@@ -0,0 +1,22 @@
+
+
+public class practice1 {
+    public static void main(String[] args) {
+        System.out.println("WELCOM TO KUNAL MALIK SCHOOL ! ");
+        System.out.println("RESULT ");
+        System.out.println();
+        int num = 23;
+
+        if (num>90)
+            System.out.println("The Grade of Studdent is A++ ");
+        
+        else if (num>75)
+            System.out.println("The grade of studdent is C++");
+        else if (num<50)
+            System.out.println("The student is Fail !");
+        else 
+            System.out.println("Thank you !");
+
+                        }
+
+}
