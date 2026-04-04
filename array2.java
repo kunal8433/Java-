a18c958300de0c2a52f5e276143e7698a25f24d4
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-04-04 23:35:47 +0530

    Add files via upload

---
+++ array2.java
@@ -0,0 +1,8 @@
+public class array2 {
+    public static void main(String[] args) {
+
+        int num1 [] = {12,3,24,43,34};
+        System.out.println("THE ARRAY IS : " + num1[3]);
+    }
+    
+}
