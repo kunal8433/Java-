be656feea68d580d02acfbc13d9649446c50cdd7
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-04-10 22:06:34 +0530

    Add files via upload

--- ps.java
+++ ps.java
@@ -1,9 +1,6 @@
 public class ps {
-    public static void main (String a[])
-    {
-        char c = 'k';
-        double num = 12;
-        System.out.println(c);
-        System.out.println(num);
-    }
+
+    
+
+    
 }
