b4230896a2444d8ea79c5d410af3ed407a67455e
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-04-11 23:58:34 +0530

    Add files via upload

--- input.java
+++ input.java
@@ -1,9 +1,11 @@
+import java.util.*;
+public class input {
+    public static void main(String[] args){
 
-public class input{
-    public static void main(String[] args) {
-
-        System.out.println("Its Works");
-        
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Your Number");
+        int n = sc.nextInt();
+        System.out.print(n);
+        sc.close();
     }
 }
