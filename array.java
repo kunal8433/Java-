cc705237a3bf272b2bc175e3c28cf0954731a0fb
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-04-11 23:58:06 +0530

    Add files via upload

--- array.java
+++ array.java
@@ -1,8 +1,28 @@
 public class array{
     public static void main(String[] args) {
+        
+        int num1 [] = {1,2,3,4,5};
+        int num2 [] = {6,7,8,9,10};
 
-        int num[] = { 12,23,34};
-        System.out.println(num[2]);
+        System.out.println(num1[0]);
+          System.out.println(num1[1]);
+            System.out.println(num1[2]);
+              System.out.println(num1[3]);
+                System.out.println(num1[4]);
+                
+              //------- Second array ------
+
+              System.out.println(num2[0]);
+          System.out.println(num2[1]);
+            System.out.println(num2[2]);
+              System.out.println(num2[3]);
+                System.out.println(num2[4]);
+
+
+
+
+
+
         
     }
 }
