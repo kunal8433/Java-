61b6a9ee37bf5500bc98a33c556375e3a7cdcf20
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-04-12 22:43:52 +0530

    Add files via upload

---
+++ MyString.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class MyString {
+    public static void main(String[] args) {
+        
+       Scanner sc = new Scanner (System.in);
+
+       System.out.println("Enter your Name :");
+       String name = sc.nextLine();
+       System.out.println("The Name of User is : " +  name );
+       sc.close();
+
+
+
+        }
+}
