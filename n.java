5a2e08a8c4fe3faf61dac2e2456da8f79fb58789
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-04-12 22:44:09 +0530

    Add files via upload

---
+++ n.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class n {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your number :");
+        int n = sc.nextInt();
+        System.out.println("The number is :" + n);
+        sc.close();
+    }
+    
+    }
