671f382899a9964e63e433ba2cb7ead97002acf4
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-04-12 22:44:41 +0530

    Add files via upload

---
+++ num.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class num{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner (System.in);
+
+        System.out.println("Enter your number Kunal Malik : ");
+        int n = sc.nextInt();
+        System.out.println("The number is : " + n);
+
+        sc.close();
+
+    }
+}
