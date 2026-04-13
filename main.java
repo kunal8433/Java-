bced6b4fe43b7f1bd51b1aa12a5bf2b3d68967a0
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-04-13 23:37:46 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter your name :");
+        String name = sc.nextLine();
+        System.out.println("The name of the user is :" + name);
+        sc.close();
+        
+    }
+}
