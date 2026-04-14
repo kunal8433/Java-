c8d7fcb64e70fb1a3a0b1ec9a4cd2b16c99f640b
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-04-14 23:10:48 +0530

    Add files via upload

--- input.java
+++ input.java
@@ -1,11 +1,15 @@
 import java.util.*;
-public class input {
-    public static void main(String[] args){
-
+public class input{
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter Your Number");
-        int n = sc.nextInt();
-        System.out.print(n);
+        System.out.println("Enter your name Sir :  " );
+        String name = sc.nextLine();
+        System.out.println("The name is : " + name);
         sc.close();
+
+        
     }
+    
+
+    
 }
