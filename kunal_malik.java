e49bf75641c4c3977b5dd8a95236296609a10416
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-04-16 23:06:05 +0530

    Add files via upload

---
+++ kunal_malik.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class kunal_malik{
+    public static void main(String[] args) {
+        
+        
+          Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your First name Mr. : ");
+        String x = sc.nextLine();
+
+        System.out.println("Enter your Surname Mr. :");
+        String y = sc.nextLine();
+
+        System.out.println("Enter Your Class : ");
+        int z = sc.nextInt();
+
+        System.out.println("Enter your Rollno. : ");
+        int p = sc.nextInt();
+
+        System.out.println("The Name is : " + (x + " " +  y ));
+        System.out.println("Class is : " + z);
+        System.out.println("Rollno is : " + p );
+
+        sc.close();
+
+
+        
+        
+    }
+    
+
+}
+
