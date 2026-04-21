05c52bc4968595a4a8ede43279409a3779fd8934
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-04-21 21:46:03 +0530

    Add files via upload

--- user_input.java
+++ user_input.java
@@ -1,19 +1,22 @@
 import java.util.*;
+
 public class user_input {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter your First number :");
-        int a = sc.nextInt();
+        System.out.println("Enter your First number : ");
+        int x = sc.nextInt();
 
-        System.out.println("Enter your Second number : ");
-        int b = sc.nextInt();
+        System.out.println("Enter your Second name : " );
+        int y = sc.nextInt();
 
-        int result = (a+b);
-        System.out.println("The Sum of a and b is :" + result);
+        int result = (x/y);
+
+        System.out.println("The Division value of x and y is :" + result);
         sc.close();
-
         
+
     }
     
 }
