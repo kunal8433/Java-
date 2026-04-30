fbc172e6c64ea96942935708ed325ae5d4e57d05
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-04-30 21:43:37 +0530

    Add files via upload

--- kunal.java
+++ kunal.java
@@ -1,11 +1,21 @@
 import java.util.*;
-public class kunal {
+public class kunal{
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
-        System.out.println("Enter your name :");
-        String name = sc.nextLine();
-        System.out.println("The name of the user is :" + name);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your First name :");
+        String x = sc.nextLine();
+
+        System.out.println("Enter your Second name : ");
+        String y = sc.nextLine();
+
+        String result = (x+y);
+        System.out.println("The Full name is :" + result);
+
         sc.close();
-        
+
+
     }
+    
+    
 }
