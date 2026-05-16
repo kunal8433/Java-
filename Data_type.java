007bdfe82a020fc9b0a5262b4061467f8bb8941e
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-05-16 22:13:01 +0530

    Add files via upload

--- Data_type.java
+++ Data_type.java
@@ -1,19 +1,24 @@
 import java.util.*;
-public class  Data_type{
+
+public class Data_type {
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter your first number :");
-        float x = sc.nextFloat();
-        System.out.println("Enter your Second number :");
-        float y = sc.nextFloat();
+        System.out.println("Enter anything:");
 
-        float result = (x+y);
-
-        System.out.println("The result is: " + result);
+        if (sc.hasNextInt()) {
+            int x = sc.nextInt();
+            System.out.println("Integer hai: " + x);
+        }
+        else if (sc.hasNextFloat()) {
+            float y = sc.nextFloat();
+            System.out.println("Float hai: " + y);
+        }
+        else {
+            String str = sc.next();
+            System.out.println("String hai: " + str);
+        }
 
         sc.close();
-        
     }
-    
 }
