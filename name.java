38da3d4e6f9c7f2ac0abd6943cdada0608efaf86
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-05-24 17:57:28 +0530

    Add files via upload

--- name.java
+++ name.java
@@ -1,19 +1,22 @@
-import java.util.*;
 public class name {
+  
     public static void main(String[] args) {
         
-        Scanner k = new Scanner (System.in);
-
-        System.out.println("Enter your first name :");
-        String a  = k.nextLine();
-
-        System.out.println("Enter your Sur name : ");
-        String  b = k.nextLine();
-
-        String result = (a + " " + b);
-        System.out.println("The full name of k is :" + result);
-
-        k.close();
+        int nums3[] = new int [4];
+        nums3[0]=1;
+        nums3[1]=2;
+        nums3[2]=3;
+        nums3[3]=4;
         
+       for(int i=0;i<4;i++)
+       {
+        System.out.println(nums3[i]);
+       }
+                 
+
     }
+    
 }
+
+    
+
