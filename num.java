f0cfafd36e493bafd720e2133c035cbbb6689da5
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-06-19 19:35:42 +0530

    Add files via upload

--- num.java
+++ num.java
@@ -1,14 +1,6 @@
-import java.util.*;
-public class num{
-    public static void main(String[] args)
-    {
-        Scanner sc = new Scanner (System.in);
-
-        System.out.println("Enter your number Kunal Malik : ");
-        int n = sc.nextInt();
-        System.out.println("The number is : " + n);
-
-        sc.close();
-
+class Solution {
+    public boolean isSameAfterReversals(int num) {
+        return num==0 || num%10!=0;
+        
     }
 }
