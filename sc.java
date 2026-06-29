1fe9b4043ddd948a4d58357ed6bbc56c910f88ea
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-06-29 18:22:34 +0530

    Add files via upload

---
+++ sc.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class sc {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x =12;
+        int y = 1;
+        int result = x+y;
+        System.out.println(result);
+        sc.close();
+    }
+    
+}
