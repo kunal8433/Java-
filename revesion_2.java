61140ae01704c6c5b7b8de0fbd1fd30c6d57c210
Kunal Chaudhary <kunalchaudhary843305@gmail.com>
2026-07-20 20:56:41 +0530

    Add files via upload

---
+++ revesion_2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class revesion_2 {
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        
+        System.out.println("Enter yourr First number : ");
+        int x  = sc.nextInt();
+
+        System.out.println("Enter your Second number :");
+        int y = sc.nextInt();
+
+        int result  = x+y;
+        System.out.println("The Sum of thise Number is :" + result);
+
+        sc.close();
+
+
+        
+    }
+    
+}
